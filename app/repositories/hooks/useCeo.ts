112aae01a769a44f2fc2c55b6cd6cbcf43bf13b3
golf-sarawut <positwinyu.sarawut@moneyforward.co.jp>
2024-07-25 10:54:29 +0900

    update.

--- app/repositories/hooks/useCeo.ts
+++ app/repositories/hooks/useCeo.ts
@@ -5,6 +5,7 @@ import { CeoQuery, CeoQueryResult } from "../queries/ceoQuery";
 export function useCeo() {
   const [result, reexecuteQuery] = useQuery<CeoQueryResult>({
     query: CeoQuery,
+    requestPolicy: "cache-and-network",
   });
 
   return {
